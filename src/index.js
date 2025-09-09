f7147b31c16565ae30e16be8890774da1f75b17f
juanRodriguezGarcia <juanRodriguezGarcia@users.noreply.github.com>
2025-09-08 20:58:34 -0500

    ggggggg

--- src/index.js
+++ src/index.js
@@ -3,3 +3,18 @@ const sum = require('./sum');
 console.log('¡Hola Mundo!');
 console.log('Suma de 2 + 3 =', sum(2, 3));
 console.log('Suma de 10 + 5 =', sum(10, 5));
+
+/* istanbul ignore next */
+// Code Smell: Duplicación de código
+/* istanbul ignore next */
+function calculateUserAge(birthYear) {
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - birthYear;
+  return age;
+}
+/* istanbul ignore next */
+function calculateEmployeeAge(birthYear) {
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - birthYear;
+  return age;
+}
--- src/vulnerabilidades.js
+++ src/vulnerabilidades.js
@@ -30,17 +30,3 @@ processUser({name:"Juan", age:30, email:"juan@mail.com", phone:"1234"});
 console.log("Nombre usuario:", getUserProperty({name:"Juan"}, "name"));
 console.log("Email usuario:", getUserProperty({email:"juan@mail.com"}, "email"));
 console.log("SHA256 hash:", hash);
-/* istanbul ignore next */
-// Code Smell: Duplicación de código
-/* istanbul ignore next */
-function calculateUserAge(birthYear) {
-  const currentYear = new Date().getFullYear();
-  const age = currentYear - birthYear;
-  return age;
-}
-/* istanbul ignore next */
-function calculateEmployeeAge(birthYear) {
-  const currentYear = new Date().getFullYear();
-  const age = currentYear - birthYear;
-  return age;
-}
